5b4fd38723b9cf208de5439d7a9b4232bc542a09
Boris Charney <boris.charney@gmail.com>
2022-09-01 14:38:41 -0700

    commit tree 4447e28fc042cef3ae1403f78bb0846af08a13e2

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+const random = (Math.round(Math.random() * 3) + 1)
+
+const num3 = 4
+const num4 = 65
+const mod = num3 % num4
+
+const max = Math.max(1, 2, 3, 20)
